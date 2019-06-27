7243597bb617312ab0e99f1aa75b53987d9a3f71
Mzoir_zakariya <47563024+mzoir@users.noreply.github.com>
2019-06-27 01:42:16 +0100

    installer.py

---
+++ installer.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import time
+
+
+time.sleep(5)
+print ' Installing......requiremenet....'
+time.sleep(1)
+print ' Installing......requiremenet......'
+time.sleep(1)
+print ' Installing......requiremenet........'
+time.sleep(1)
+print ' Installing......requiremenet.........'
+time.sleep(1)
+print ' Installing......requiremenet...........'
+
+os.system('pip2 install mechanize wordlist ')
+print ' Typing..... (python2 newt.py)'
+os.system('python2 newt.py')
